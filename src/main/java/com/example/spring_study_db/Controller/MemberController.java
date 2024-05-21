192ce6d6b3f786d6feeaff2d20d3b4b80b29a34e
아이스바닐라라떼 <121101681+ljw971125@users.noreply.github.com>
2024-05-21 13:26:48 +0900

    회원가입(DB X)

---
+++ src/main/java/com/example/spring_study_db/Controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.example.spring_study_db.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MemberController {
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/spring_study_db/entity/MemberEntity.java
@@ -0,0 +1,4 @@
+package com.example.spring_study_db.entity;
+
+public class MemberEntity {
+}
